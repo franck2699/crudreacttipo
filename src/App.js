93c444da762588e5f388fa52df5a138e2389fb85
franc_000 <franc_000@LIIN-PC>
2020-11-17 19:35:36 -0600

    make it better

--- src/App.js
+++ src/App.js
@@ -1,25 +1,157 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const url="http://localhost:8080/tipo";
+
+class App extends Component {
+  state={
+    data:[],
+    modalInsertar: false,
+    modalEliminar: false,
+    form:{
+      id: '',
+      valor: ''
+    }
+  }
+  
+  peticionGet=()=>{
+  axios.get(url +"/listar").then(response=>{
+    this.setState({data: response.data});
+  }).catch(error=>{
+    console.log(error.message);
+  })
+  }
+  
+  peticionPost=async()=>{
+    delete this.state.form.id;
+   await axios.post(url,this.state.form).then(response=>{
+      this.modalInsertar();
+      this.peticionGet();
+    }).catch(error=>{
+      console.log(error.message);
+    })
+  }
+  
+  peticionPut=()=>{
+    axios.put(url+"/"+this.state.form.id, this.state.form).then(response=>{
+      this.modalInsertar();
+      this.peticionGet();
+    })
+  }
+  
+  peticionDelete=()=>{
+    axios.delete(url+"/"+this.state.form.id).then(response=>{
+      this.setState({modalEliminar: false});
+      this.peticionGet();
+    })
+  }
+  
+  modalInsertar=()=>{
+    this.setState({modalInsertar: !this.state.modalInsertar});
+  }
+  
+  seleccionartipo=(tipo)=>{
+    this.setState({
+      tipoModal: 'actualizar',
+      form: {
+        id: tipo.id,
+        valor: tipo.valor,
+      }
+    })
+  }
+  
+  handleChange=async e=>{
+  e.persist();
+  await this.setState({
+    form:{
+      ...this.state.form,
+      [e.target.name]: e.target.value
+    }
+  });
+  console.log(this.state.form);
+  }
+  
+    componentDidMount() {
+      this.peticionGet();
+    }
+    
+  
+    render(){
+      const {form}=this.state;
+    return (
+      <div className="App">
+      <br /><br /><br />
+    <button className="btn btn-success" onClick={()=>{this.setState({form: null, tipoModal: 'insertar'}); this.modalInsertar()}}>Agregar Tipo</button>
+    <br /><br />
+      <table className="table ">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>tipo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.data.map(tipo=>{
+            return(
+              <tr>
+            <td>{tipo.id}</td>
+            <td>{tipo.valor}</td>
+            <td>
+                  <button className="btn btn-primary" onClick={()=>{this.seleccionartipo(tipo); this.modalInsertar()}}></button>
+                  {"   "}
+                  <button className="btn btn-danger" onClick={()=>{this.seleccionartipo(tipo); this.setState({modalEliminar: true})}}></button>
+                  </td>
+            </tr>
+            )
+          })}
+        </tbody>
+      </table>
+  
+      <Modal isOpen={this.state.modalInsertar}>
+                  <ModalHeader style={{display: 'block'}}>
+                    <span style={{float: 'right'}} onClick={()=>this.modalInsertar()}>x</span>
+                  </ModalHeader>
+                  <ModalBody>
+                    <div className="form-group">
+                      <label htmlFor="id">ID</label>
+                      <input className="form-control" type="text" name="id" id="id" readOnly onChange={this.handleChange} value={form?form.id: this.state.data.length+1}/>
+                      <br />
+                      <label htmlFor="valor">tipo</label>
+                      <input className="form-control" type="text" name="valor" id="valor" onChange={this.handleChange} value={form?form.valor: ''}/>
+                      <br />
+            </div>
+                  </ModalBody>
+  
+                  <ModalFooter>
+                    {this.state.tipoModal=='insertar'?
+                      <button className="btn btn-success" onClick={()=>this.peticionPost()}>
+                      Insertar
+                    </button>: <button className="btn btn-primary" onClick={()=>this.peticionPut()}>
+                      Actualizar
+                    </button>
+    }
+                      <button className="btn btn-danger" onClick={()=>this.modalInsertar()}>Cancelar</button>
+                  </ModalFooter>
+            </Modal>
+  
+  
+            <Modal isOpen={this.state.modalEliminar}>
+              <ModalBody>
+                 Estás seguro que deseas eliminar al tipo {form && form.valor}
+              </ModalBody>
+              <ModalFooter>
+                <button className="btn btn-danger" onClick={()=>this.peticionDelete()}>Sí</button>
+                <button className="btn btn-secundary" onClick={()=>this.setState({modalEliminar: false})}>No</button>
+              </ModalFooter>
+            </Modal>
     </div>
-  );
-}
-
-export default App;
+  
+  
+  
+    );
+  }
+  }
+  export default App;
